5d2f16fe5577756f15881be40cf1c119bf7350cd
ahmedibrahim240 <ahmedibrahim150240@gmail.com>
2024-10-26 14:18:37 +0300

    add remove splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MovesApp());
 }
 
@@ -10,11 +13,11 @@ class MovesApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    FlutterNativeSplash.remove();
     return const MaterialApp(
       title: 'Moves',
       debugShowCheckedModeBanner: false,
       home: Scaffold(),
-     
     );
   }
 }
